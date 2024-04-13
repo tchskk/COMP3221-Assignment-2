d21e01ce25c9f58234f3e8ab821db4e6d3a63e3f
tchskk <theochands@gmail.com>
2024-04-13 16:55:30 +1000

    Commit #3

--- COMP3221_FLClient.py
+++ COMP3221_FLClient.py
@@ -6,13 +6,44 @@ import select
 import torch
 import torch.nn as nn
 
+import numpy as np
+
 class Client:
     def __init__(self, client_id, port, opt_method):
         self.client_id = client_id
         self.port = port
         self.opt_method = opt_method
-        self.training_dataset = None
-        self.testing_dataset = None
+        self.X_train = None
+        self.y_train = None
+        self.X_test = None
+        self.y_test = None
+        self.model = None
+        self.loss = nn.MSELoss()
+        self.learning_rate = 0.01
+        self.epochs = 5
+
+    def train(self):
+        LOSS = 0
+        self.model.train()
+        for epoch in range(self.epochs):
+            self.model.train()
+            for batch_idx, (X, y) in enumerate(self.trainloader):
+                self.optimizer.zero_grad()
+                output = self.model(X)
+                loss = self.loss(output, y)
+                loss.backward()
+                self.optimizer.step()
+        return loss.data
+
+    def test(self):
+        self.model.eval()
+        mse = 0
+        for X, y in self.testing_dataset:
+            y_pred = self.model(x)
+            # Calculate evaluation metrics
+            mse += self.loss(y_pred, y)
+            print(str(self.id) + ", MSE of client ",self.id, " is: ", mse)
+        return mse
 
     def listen(self):
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
@@ -52,36 +83,52 @@ class Client:
         return s
     
     def load_dataset(self):
-        training_dataset = []
-        testing_dataset = []
+        X_train = None
+        y_train = None
+        X_test = None
+        y_test = None
 
         f = open(f"./FLData/calhousing_train_{self.client_id}.csv", 'r')
         f.readline()
         lines = f.read().split('\n')
-        training_dataset = [0]*len(lines)
+        X_train = []
+        y_train = []
         f.close()
         for i in range(len(lines)):
             splitted = lines[i].split(",")
             if len(splitted) == 9:
-                training_dataset[i] = list(map(float, splitted))
-
+                X_train.append([float(i) for i in splitted[:8]])
+                y_train.append(float(splitted[8]))
+        X_train = np.array(X_train)
+        y_train = np.array(y_train)
+                
         f = open(f"./FLData/calhousing_test_{self.client_id}.csv", 'r')
         f.readline()
         lines = f.read().split('\n')
-        testing_dataset = [0]*len(lines)
+        X_test = []
+        y_test = []
         f.close()
         for i in range(len(lines)):
             splitted = lines[i].split(",")
             if len(splitted) == 9:
-                testing_dataset[i] = list(map(float, splitted))
+                X_test.append([float(i) for i in splitted[:8]])
+                y_test.append(float(splitted[8]))
+        X_test = np.array(X_test)
+        y_test = np.array(y_test)
 
-        return training_dataset, testing_dataset
+        X_train = torch.Tensor(X_train).view(-1,1).type(torch.float32)
+        y_train = torch.Tensor(y_train).type(torch.float32)
+        X_test = torch.Tensor(X_test).view(-1,1).type(torch.float32)
+        y_test = torch.Tensor(y_test).type(torch.float32)
+
+        return X_train, y_train, X_test, y_test
 
     def start(self):
-        self.training_dataset, self.testing_dataset = self.load_dataset()
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        self.X_train, self.y_train, self.X_test, self.y_test = self.load_dataset()
+        
+        """with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect(('localhost', 6000))
-            s.sendall(b'Hello Boss')
+            s.sendall(b'Hello Boss')"""
             
 
 if __name__ == "__main__":
--- COMP3221_FLServer.py
+++ COMP3221_FLServer.py
@@ -1,37 +1,82 @@
 import sys
 import socket
-import time
 import json
 import select
 
-def load_config(filename):
-    # neighbours = {} of "(node_id, port)" -> (Node object, cost)
-    # (new) neigbours = {'neighbour': {'cost': cost, 'port': port}}
-    neighbours = {}
-    f = open(filename, 'r')
-    f.readline()
-    lines = f.read().split('\n')
-    for line in lines:
-        word = line.split(" ")
-        node = word[0]
-        cost = float(word[1])
-        port = int(word[2])
-        neighbours[node] = {'cost': cost, 'port': port, 'heartbeat': 11}
-    return neighbours
+import torch
+import torch.nn as nn
 
-def main(node, port, neighbours):
-    node = Node(node, port, neighbours)
-    node.start()
+class Server:
+    def __init__(self, port, subsampling):
+        self.port = port
+        self.subsampling = subsampling
+        self.num_glob_iters = 100 # T
+        self.global_model = None
+        self.clients = {}
+        self.no_clients = 0
+
+    class LinearRegressionModel(nn.Module):
+        def __init__(self, input_size = 8):
+            super(self.__class__, self).__init__()
+            # Create a linear transformation to the incoming data
+            self.linear = nn.Linear(input_size, 1)
+
+        # Define how the model is going to be run, from input to output
+        def forward(self, x):
+            # Apply linear transformation
+            output = self.linear(x)
+            return output.reshape(-1)
+
+    def listen(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+            s.bind(('localhost', self.port))
+            s.setblocking(0)
+            while not self.stop_listen:
+                ready = select.select([s], [], [], 1)
+                if ready[0]:
+                    packet = s.recv(1024)
+                    if self.active:
+                        self.process_packet(json.loads(packet.decode('utf-8')))
+
+    def process_packet(self, decoded_packet: dict):
+        # packet = {  }
+        pass
+
+    def send_to(self, s):
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+            packet = self.generate_packet()
+            encoded_packet = json.dumps(packet).encode('utf-8')
+            s.sendto(encoded_packet, ('localhost', port))
+
+    def generate_packet(self):
+        # packet format
+        # packet = {  }
+        packet = {}
+        return packet
     
+    def initialize_connection(self):
+        # Connect to Server
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect(('localhost', 6000))
+                s.sendall(b'Hello Boss')
+        except:
+            print("Can't connect to the Server")
+        return s
 
+    def start(self):
+        self.global_model = self.LinearRegressionModel()
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind(('localhost', 6000))
+            s.listen(5)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    # python COMP3221_FLClient.py <Client-id> <Port-Client> <Opt-Method>
+    if len(sys.argv) != 3:
         print("Wrong arguments")
         sys.exit(1)
 
-    node = sys.argv[1]
-    port = int(sys.argv[2])
-    filename = sys.argv[3]
-    neighbours = load_config(filename)
-    main(node, port, neighbours)
+    port = int(sys.argv[1])
+    subsampling = sys.argv[3]
+    server = Server(port, subsampling)
+    server.start()
