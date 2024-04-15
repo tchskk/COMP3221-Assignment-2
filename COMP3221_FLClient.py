ffafde7e46e1540597f529e9d659f26f9efd7c02
tchskk <theochands@gmail.com>
2024-04-16 04:38:13 +1000

    Commit #5

--- COMP3221_FLClient.py
+++ COMP3221_FLClient.py
@@ -1,7 +1,7 @@
 import sys
 import socket
-import json
-import select
+import threading
+import pickle
 
 import torch
 import torch.nn as nn
@@ -9,20 +9,6 @@ from torch.utils.data import DataLoader
 
 import numpy as np
 import copy
-
-class LinearRegressionModel(nn.Module):
-    def __init__(self, input_size = 8):
-        super(self.__class__, self).__init__()
-        # Create a linear transformation to the incoming data
-        self.linear = nn.Linear(input_size, 1)
-
-    # Define how the model is going to be run, from input to output
-    def forward(self, x):
-        # Apply linear transformation
-        output = self.linear(x)
-        return output.reshape(-1)
-
-initialize_model = LinearRegressionModel()
 
 class Client:
     def __init__(self, client_id, port, opt_method):
@@ -35,82 +21,73 @@ class Client:
         self.test_data = [(x, y) for x, y in zip(self.X_test, self.y_test)]
 
         # Define dataloader for iterable sample over a dataset
-        self.batch_size = 25
+        self.batch_size = 64
         if self.opt_method == 0:
             self.batch_size = len(self.train_data)
         self.trainloader = DataLoader(self.train_data, batch_size = self.batch_size)
         self.testloader = DataLoader(self.test_data, batch_size = len(self.test_data))
 
-        self.model = copy.deepcopy(initialize_model)
+        self.model = self.LinearRegressionModel()
         self.loss = nn.MSELoss()
-        self.learning_rate = 0.01
-        self.epochs = 5
+        self.learning_rate = 0.00025
+        self.epochs = 2
         self.optimizer = torch.optim.SGD(self.model.parameters(), lr=self.learning_rate)
+    
+    class LinearRegressionModel(nn.Module):
+        def __init__(self, input_size = 8):
+            super(self.__class__, self).__init__()
+            self.linear = nn.Linear(input_size, 1)
 
-    def set_parameters(self, model):
-        for old_param, new_param in zip(self.model.parameters(), model.parameters()):
-            old_param.data = new_param.data.clone()
+        def forward(self, x):
+            output = self.linear(x)
+            return output.reshape(-1)
+
+    def update_model(self, global_model_state):
+        self.model.load_state_dict(global_model_state)
 
     def train(self):
-        loss = 0
         self.model.train()
+        total_loss = 0
+        total_batches = 0
         for epoch in range(self.epochs):
-            self.model.train()
-            for batch_idx, (X, y) in enumerate(self.trainloader):
+            epoch_loss = 0
+            batch_count = 0
+            for _, (x, y) in enumerate(self.trainloader):
                 self.optimizer.zero_grad()
-                output = self.model(X)
+                output = self.model(x)
                 loss = self.loss(output, y)
                 loss.backward()
+                torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1.0)
                 self.optimizer.step()
-        return loss.data
+                epoch_loss += loss.item()
+                batch_count += 1
+            epoch_loss = epoch_loss / batch_count
+            total_loss += epoch_loss
+            total_batches += 1
+        average_loss = total_loss / total_batches
+        return average_loss
 
+    def log_result(self, mse_train, mse_test, round):
+        mse_train = self.train()
+        mse_test = self.test()
+        log_file = f"{self.client_id}_log.txt"
+        with open(log_file, 'a') as f:
+            f.write(f"Global Iteration: {str(round)}\n")
+            f.write(f"Training MSE: {str(mse_train)}\n")
+            f.write(f"Testing MSE: {str(mse_test)}\n\n")
+    
     def test(self):
         self.model.eval()
         mse = 0
-        for X, y in self.testing_dataset:
-            y_pred = self.model(X)
-            # Calculate evaluation metrics
-            mse += self.loss(y_pred, y)
-            print(str(self.id) + ", MSE of client ",self.id, " is: ", mse)
+        total_batches = 0
+        for x, y in self.testloader:
+            y_pred = self.model(x)
+            mse += self.loss(y_pred, y).item()
+            total_batches += 1
+        if total_batches > 0:
+            mse = mse / total_batches
         return mse
 
-    def listen(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-            s.bind(('localhost', self.port))
-            s.setblocking(0)
-            while not self.stop_listen:
-                ready = select.select([s], [], [], 1)
-                if ready[0]:
-                    packet = s.recv(1024)
-                    if self.active:
-                        self.process_packet(json.loads(packet.decode('utf-8')))
-
-    def process_packet(self, decoded_packet: dict):
-        # packet = {  }
-        pass
-
-    def send_to(self, s):
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-            packet = self.generate_packet()
-            encoded_packet = json.dumps(packet).encode('utf-8')
-            s.sendto(encoded_packet, ('localhost', port))
-
-    def generate_packet(self):
-        # packet format
-        # packet = {  }
-        packet = {}
-        return packet
-    
-    def initialize_connection(self):
-        # Connect to Server
-        try:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.connect(('localhost', 6000))
-                s.sendall(b'Hello Boss')
-        except:
-            print("Can't connect to the Server")
-        return s
-    
     def load_dataset(self):
         X_train = None
         y_train = None
@@ -145,18 +122,39 @@ class Client:
         X_test = np.array(X_test)
         y_test = np.array(y_test)
 
-        X_train = torch.Tensor(X_train).view(-1,1).type(torch.float32)
+        X_train = torch.Tensor(X_train).type(torch.float32)
         y_train = torch.Tensor(y_train).type(torch.float32)
-        X_test = torch.Tensor(X_test).view(-1,1).type(torch.float32)
+        X_test = torch.Tensor(X_test).type(torch.float32)
         y_test = torch.Tensor(y_test).type(torch.float32)
 
         return X_train, y_train, X_test, y_test
 
     def start(self):
-        """with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect(('localhost', 6000))
-            s.sendall(b'Hello Boss')"""
-            
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.bind(('localhost', self.port))
+        client.connect(('localhost', 6000))
+        print("connected to server")
+        packet = {'client_id': self.client_id, 'sample_size': len(self.train_data)}
+        encoded_packet = pickle.dumps(packet)
+        client.send(encoded_packet)
+        count = 0
+        while True:
+            print(f"I am {self.client_id}")
+            global_model_state = pickle.loads(client.recv(4096))
+            if global_model_state == "finished":
+                break
+            print("Received new global model")
+            self.update_model(global_model_state)
+            mse = self.test()
+            print(f"Testing MSE: {str(mse)}")
+            print("Local training ...")
+            loss = self.train()
+            self.log_result(loss, mse, count+1)
+            print(f"Training MSE: {str(loss)}")
+            print("Sending new local model")
+            client.send(pickle.dumps(self.model.state_dict()))
+            count+=1
+        client.close()
 
 if __name__ == "__main__":
     # python COMP3221_FLClient.py <Client-id> <Port-Client> <Opt-Method>
--- COMP3221_FLServer.py
+++ COMP3221_FLServer.py
@@ -1,19 +1,25 @@
 import sys
+import threading
+import time
 import socket
-import json
-import select
+import pickle
 
 import torch
 import torch.nn as nn
 
+import copy
+
 class Server:
     def __init__(self, port, subsampling):
         self.port = port
-        self.subsampling = subsampling
-        self.num_glob_iters = 100 # T
-        self.global_model = None
+        self.subsampling = subsampling #0 means all clients, 0-5
+        self.num_glob_iters = 1000000 # T
+        self.global_model = self.LinearRegressionModel()
         self.clients = {}
-        self.no_clients = 0
+        self.client_ids = []
+        self.total_datasize = 0
+
+        self.first_handshake = False
 
     class LinearRegressionModel(nn.Module):
         def __init__(self, input_size = 8):
@@ -27,48 +33,82 @@ class Server:
             output = self.linear(x)
             return output.reshape(-1)
 
-    def listen(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-            s.bind(('localhost', self.port))
-            s.setblocking(0)
-            while not self.stop_listen:
-                ready = select.select([s], [], [], 1)
-                if ready[0]:
-                    packet = s.recv(1024)
-                    if self.active:
-                        self.process_packet(json.loads(packet.decode('utf-8')))
+    def listen(self, server:socket.socket):
+        server.listen(5)
+        while len(self.client_ids) < 5:
+            conn,_ = server.accept()
+            print("accepted client")
+            if len(self.client_ids) == 0:
+                self.first_handshake = True
+            packet = conn.recv(1024)
+            client_id, sample_size = self.process_packet(pickle.loads(packet))
+            self.client_ids.append(client_id)
+            self.clients[client_id] = {'connection': conn, 'sample_size': sample_size}
+            self.total_datasize += sample_size
 
     def process_packet(self, decoded_packet: dict):
-        # packet = {  }
-        pass
+        # packet = { 'client_id': 'client1', 'batch_size': int() }
+        return decoded_packet['client_id'], int(decoded_packet['sample_size'])
 
-    def send_to(self, s):
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-            packet = self.generate_packet()
-            encoded_packet = json.dumps(packet).encode('utf-8')
-            s.sendto(encoded_packet, ('localhost', port))
-
-    def generate_packet(self):
+    def send_global_model(self, client_connection):
         # packet format
         # packet = {  }
-        packet = {}
-        return packet
-    
-    def initialize_connection(self):
-        # Connect to Server
-        try:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.connect(('localhost', 6000))
-                s.sendall(b'Hello Boss')
-        except:
-            print("Can't connect to the Server")
-        return s
+        client_connection.send(pickle.dumps(self.global_model.state_dict()))
+
+    def aggregate_parameters(self, client_model_states: dict):
+        global_state_dict = self.global_model.state_dict()
+        for key in global_state_dict.keys():
+            global_state_dict[key] = torch.zeros_like(global_state_dict[key])
+
+        for client_id, local_state_dict in client_model_states.items():
+            for key in global_state_dict.keys():
+                contribution_scale = self.clients[client_id]['sample_size'] / self.total_datasize
+                global_state_dict[key] += local_state_dict[key] * contribution_scale
+
+        self.global_model.load_state_dict(global_state_dict)
 
     def start(self):
-        self.global_model = self.LinearRegressionModel()
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind(('localhost', 6000))
-            s.listen(5)
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(('localhost', self.port))
+        listening_thread = threading.Thread(target = self.listen, args=(server,))
+        listening_thread.daemon = True
+        listening_thread.start()
+        while not self.first_handshake:
+            pass
+        self.first_handshake = False
+        time.sleep(10)
+
+        global_count = 0
+        while global_count < self.num_glob_iters:
+            count = 0
+            client_models = {}
+            print("Broadcasting new global model")
+            for client_id in self.client_ids:
+                conn = self.clients[client_id]['connection']
+                conn.send(pickle.dumps(self.global_model.state_dict()))
+                #print(self.global_model.state_dict())
+                count += 1
+            print(f"Global Iteration {global_count}")
+            print(f"Total Number of clients: {count}")
+            for i in range(count):
+                # packet = {'client_id': 'client1, 'local_model': nn.Module, ''}
+                client_id = self.client_ids[i]
+                print(f"Getting local model from {client_id}")
+                conn = self.clients[client_id]['connection']
+                local_model = pickle.loads(conn.recv(4096))
+                #print(local_model)
+                client_models[client_id] = local_model
+
+            self.aggregate_parameters(client_models)
+            global_count += 1
+
+        for client_id in self.client_ids:
+            message = "finished"
+            conn = self.clients[client_id]['connection']
+            conn.send(pickle.dumps(message))
+            conn.close()
+
+        server.close()
 
 if __name__ == "__main__":
     # python COMP3221_FLClient.py <Client-id> <Port-Client> <Opt-Method>
@@ -77,6 +117,6 @@ if __name__ == "__main__":
         sys.exit(1)
 
     port = int(sys.argv[1])
-    subsampling = sys.argv[3]
+    subsampling = int(sys.argv[2])
     server = Server(port, subsampling)
     server.start()
